b1e9f27c1f7fa96597b60d0b51d2e3ba63a26d0e
sowmiya28ga <gasowmiya28@gmail.com>
2025-09-29 09:52:20 +0530

    program21.py

---
+++ program21.py
@@ -0,0 +1,8 @@
+print("Square Pattern")
+print("-----------------------")
+n = int(input("Enter The Number: "))
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print() 
+
