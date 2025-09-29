43d225a3aaa71f63d7170bf04c7f4e239d839073
sowmiya28ga <gasowmiya28@gmail.com>
2025-09-29 10:03:31 +0530

    program22.py

---
+++ program22.py
@@ -0,0 +1,7 @@
+print("Triangle Pattern")
+print("----------------------")
+n = int(input("Enter The Number: "))
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()  
