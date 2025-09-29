52991b4c179ae150537f043ff20afef385bbc783
sowmiya28ga <gasowmiya28@gmail.com>
2025-09-29 10:25:47 +0530

    program23.py

---
+++ program23.py
@@ -0,0 +1,8 @@
+print("Diamond Pattern")
+print("----------------")
+n = int(input("Enter The Number: "))
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
