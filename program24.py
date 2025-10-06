53998cc02bbe3ada45096ffc68ded6180b4a6fb9
sowmiya28ga <gasowmiya28@gmail.com>
2025-10-06 10:26:38 +0530

    program24.py

---
+++ program24.py
@@ -0,0 +1,8 @@
+print("Right Arrow Pattern")
+print("----------------------")
+
+n = int(input("Enter The Number: "))
+for i in range(1, n + 1):
+    print("* " * i)
+for i in range(n - 1, 0, -1):
+    print("* " * i)
